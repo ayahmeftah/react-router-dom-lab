b4b70b128f6163c5cd1a54c1377290707fe0b8d6
ayahmeftah <ayahmeftah123123@gmail.com>
2025-08-11 23:05:30 +0300

    added navigation to mailbox form

--- src/components/MailboxForm/MailboxForm.jsx
+++ src/components/MailboxForm/MailboxForm.jsx
@@ -1,11 +1,18 @@
-
+import { useNavigate } from "react-router"
 
 const MailboxForm = (props) => {
+    const navigate = useNavigate()
+
+    const handleBoxSubmit = (event) => {
+        event.preventDefault()
+        props.addBox(event)
+        navigate('/mailboxes')
+    }
 
     return(
         <>
             <h1>New Mailbox</h1>
-            <form onSubmit={props.addBox}>
+            <form onSubmit={handleBoxSubmit}>
                 <label>Enter a Boxholder:</label>
                 <input type="text" name="boxOwner" placeholder="Boxholder name" onChange={props.handleInputChange} value={props.newMailbox.boxOwner}/>
                 <label>Select a Box Size</label>
